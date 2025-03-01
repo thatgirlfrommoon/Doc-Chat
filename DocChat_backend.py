e09da7aa7cdef16e25429b2ebfeff1aa081e067a
Harithac1991 <30717046+username@users.noreply.github.com>
2025-03-01 13:33:57 +0530

    adding tracked files

---
+++ DocChat_backend.py
@@ -0,0 +1,82 @@
+import chromadb
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = openai.OpenAI()
+
+# Function to get embedidngs from openai
+def get_embedding(text):
+    """ Create text embeddings."""
+    print("creating embedding...")
+    response = client.embeddings.create(
+        input=text,
+    model=os.getenv("OPENAI_EMBEDDING_MODEL"),
+
+    )
+    if not response.data[0].embedding:
+        print("Failed to create embedding")
+    print("Created embedding!")
+    return response.data[0].embedding
+    
+
+def get_collection():
+    """Initialise the chroma db client."""
+    # initialise chromadb
+    client = chromadb.PersistentClient(path='./VectorDB/vectorstore')
+    collection = client.get_or_create_collection("crawled_docs_1")
+    return collection
+
+def generate_response_from_vectordb(user_query):
+    """Create embeddings for user query and find response from vector store."""
+
+    collection = get_collection()
+    query_embedding = get_embedding(user_query)
+    collection_results =  collection.query(
+        query_embeddings = [query_embedding],
+        n_results = 2
+    )
+    print("collection_results  : ",collection_results["documents"][0]) 
+    return (collection_results["documents"][0]) if collection_results["documents"] else ""
+
+
+def chat_with_bot(user_input, conversation_history):
+    """Create prompts to converse with llm."""
+
+    retrieved_docs = generate_response_from_vectordb(user_input)
+    # print("retrieved_docs   : ", retrieved_docs)
+    context = "---- context ----\n".join(retrieved_docs)
+    
+
+    prompt = f""" 
+    You are a helpful assistant.
+    Please answer the question from the user based on the context given below. 
+    Use only the information provided in the context.
+    If you don't know the answer, you can say "I don't know".
+    -----------------------------------------------------------------------------------------------------
+    # Question
+    {user_input}
+    ------------------------------------------------------------------------------------------------------
+    # Context
+    {context}
+    ------------------------------------------------------------------------------------------------------
+    # Answer
+
+    """
+    print(prompt)
+    # Send the prompt to OpenAI's chat-based model (e.g., gpt-3.5-turbo, gpt-4)
+    response = client.chat.completions.create(
+        model=os.getenv("OPENAI_MODEL"),  # Or use "gpt-4" for GPT-4 model
+        messages=conversation_history + [{"role": "user", "content": prompt}],
+        temperature=0.1,  # Control randomness of the response (higher = more random)
+        max_tokens=150,  # Limit on tokens per response
+    )
+
+    # Extract the assistant's reply
+    bot_message = response.choices[0].message.content
+    return bot_message
+
+
+if __name__ == "__main__":
+    chat_with_bot()
---
+++ VectorDB/create_vector_store.py
@@ -0,0 +1,74 @@
+import chromadb
+from openai import OpenAI
+from langchain_text_splitters import TokenTextSplitter
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Function to get embedidngs from openai
+def get_embedding(text):
+    """ Create text embeddings."""
+    print("Creating embedding...")
+    client = OpenAI()
+    response = client.embeddings.create(
+        input=text,
+        model=os.getenv("OPENAI_EMBEDDING_MODEL")
+
+    )
+    if not response.data[0].embedding:
+        print("Failed to create embedding")
+    print("Created embedding!")
+    return response.data[0].embedding
+    
+
+def get_collection():
+    """Initialise the chroma db client."""
+    # initialise chromadb 
+    client = chromadb.PersistentClient(path='./vectorstore')
+    collection = client.get_or_create_collection("crawled_docs_1")
+    return collection
+
+def store_embedding(embedding, chunk, id):
+    """Store the embeddings"""
+    collection = get_collection()
+    collection.add(
+        embeddings=[embedding],
+        documents=[chunk],
+        metadatas=[{"source": f"chunk_{id}"}],
+        ids=[f"doc_{id}"]
+    )
+    print(f"stored chunk : {id}" )
+
+# Create embeddings
+def create_and_store_embeddings():
+    """ Create and store text embeddings."""
+
+
+    #load text file 
+    dir_path = "./../scraped_files/"
+    txt_files = [f for f in os.listdir(dir_path) if f.endswith('.txt')]
+    if len(txt_files) != 1:
+        raise ValueError('Should be only one txt file in the current directory')
+    filename = txt_files[0]
+    with open(dir_path+filename, encoding="utf-8") as file:
+        doc_text = file.read()
+    
+    # split text into chunks
+    chunk_size = 500
+    text_splitter = TokenTextSplitter(chunk_size=chunk_size,
+                                      chunk_overlap=100,)
+    chunks = text_splitter.split_text(doc_text)
+
+    # create embeddings for each chunk
+    for id, chunk in enumerate(chunks):
+        # vectorize each chunk and store
+        print(len(chunk))
+        embedding = get_embedding(chunk)
+        store_embedding(embedding,chunk, id)
+
+if __name__ == "__main__":
+    create_and_store_embeddings()
+    print("Created vector embeddings and stored in a chromadb collection...")
+
+
