5dbcd61ba51e0a59375b8a1359c436d061a745a8
Harithac1991 <30717046+username@users.noreply.github.com>
2025-03-23 08:56:59 +0530

    added Knowlede dropdown and fixed vectorisation step

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import DocChat_backend
-from crawling_pipeline import crawl_pipeline, db_pipeline
+from pipeline import crawl_pipeline, db_pipeline
+import csv
 
 st.set_page_config(page_title="Doc-Chat", page_icon="🤖", layout="wide") 
 
@@ -23,11 +24,11 @@ def create_streamlit_interface():
                     st.success("Read the website!")
                     db_status = db_pipeline()
                     if db_status["process"]:
-                        st.success("Added content to Database!")
+                        st.success("Added website to Database!")
                     else:
-                        st.error(f'{db_status["error_message"]}')
+                        st.error(f'Data base update failed: {db_status["error_message"]}')
                 else:
-                    st.success("Already added!")
+                    st.success("Website is already added!")
 
     # Display the chat message
     if 'messages' not in st.session_state:
@@ -37,10 +38,18 @@ def create_streamlit_interface():
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
+    # sidebar contents
     st.sidebar.header("Model parameter")
     st.sidebar.write("Customize the chatbot’s behavior by adjusting the settings below.")
     temp = st.sidebar.slider("Temperature", 0.0, 1.0, 0.1, key="temperature")
     max_tokens = st.sidebar.slider("Max tokens", 50, 500, 150, key="max_tokens")
+    db_data=[]
+    with open("./scraped_files/urls.csv", mode='r', newline='', encoding='utf-8') as file:
+                csv_reader = csv.DictReader(file)
+                for row in csv_reader:
+                    db_data.append(row["url"])
+
+    st.sidebar.selectbox("Knowledge base", db_data)
 
     if prompt := st.text_input("Hi👋, What's up🧑‍💻?"):
         
